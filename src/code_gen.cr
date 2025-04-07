39a8c5b0d0914b34cf6b1d4bb83c1fb4f7710518
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-04-07 10:46:09 +0200

    Formatting

--- src/code_gen.cr
+++ src/code_gen.cr
@@ -1,99 +1,98 @@
 class CodeGen
-    @embedded_c_code : String = 
-        {{ read_file("./c_code/prebuilt_code.c")}} + "\n\n"
-    @ast : Array(Rule)
+  @embedded_c_code : String = {{ read_file("./c_code/prebuilt_code.c") }} + "\n\n"
+  @ast : Array(Rule)
 
-    @output_code : String
+  @output_code : String
 
-    def initialize(@ast)
-        @output_code = @embedded_c_code
-    end
+  def initialize(@ast)
+    @output_code = @embedded_c_code
+  end
 
-    def emit(code)
-        @output_code += code + "\n"
-    end
+  def emit(code)
+    @output_code += code + "\n"
+  end
 
-    def gen_names
-        emit "const char* stacks_names[] = {"
-        emit (0...StackSymbol.counter).map { |interned| "    " + StackSymbol.get_name(interned).inspect }.join(",\n")
-        emit "};"
-        emit ""
-        emit "const char* symbols_names[] = {"
-        emit (0...SimpleSymbol.counter).map { |interned| "    " + SimpleSymbol.get_name(interned).inspect }.join(",\n")
-        emit "};"
-        emit ""
-    end
+  def gen_names
+    emit "const char* stacks_names[] = {"
+    emit (0...StackSymbol.counter).map { |interned| "    " + StackSymbol.get_name(interned).inspect }.join(",\n")
+    emit "};"
+    emit ""
+    emit "const char* symbols_names[] = {"
+    emit (0...SimpleSymbol.counter).map { |interned| "    " + SimpleSymbol.get_name(interned).inspect }.join(",\n")
+    emit "};"
+    emit ""
+  end
 
-    def gen_stacks_init
-        emit "    INIT_STACKS(stacks);"
-        @ast.each { |rule| 
-            if rule.lhs.value.empty?
-                rule.rhs.value.each { | stack_and_symbol|
-                stack, symbol = stack_and_symbol
-                emit "    stack_push(&stacks[#{stack.interned}] /* #{stack.value} */, #{symbol.interned} /* #{symbol.value} */);"
-            }
-            end
+  def gen_stacks_init
+    emit "    INIT_STACKS(stacks);"
+    @ast.each { |rule|
+      if rule.lhs.value.empty?
+        rule.rhs.value.each { |stack_and_symbol|
+          stack, symbol = stack_and_symbol
+          emit "    stack_push(&stacks[#{stack.interned}] /* #{stack.value} */, #{symbol.interned} /* #{symbol.value} */);"
         }
-        emit ""
-    end
+      end
+    }
+    emit ""
+  end
 
-    def gen_rule(n, lhs, rhs)
-        emit "    // rule #{n}"
-        #@output_code += lhs.to_s + "\n"
+  def gen_rule(n, lhs, rhs)
+    emit "    // rule #{n}"
+    # @output_code += lhs.to_s + "\n"
 
-        stack_depths = Array.new(StackSymbol.counter, 0)
+    stack_depths = Array.new(StackSymbol.counter, 0)
 
-        condition = lhs.map{ |stack_and_symbol|
-            stack, symbol = stack_and_symbol
-            depth = stack_depths[stack.interned]
-            stack_depths[stack.interned] += 1
-            "stack_peek(&stacks[#{stack.interned}] /* #{stack.value} */, #{depth}) == #{symbol.interned} /* #{symbol.value} */"
-        }.join("\n          && ")
-        emit "    if(#{condition}) {"
-        stack_depths.each_with_index { |depth, i| 
-            if depth != 0
-                emit "        stack_pop(&stacks[#{i}], #{depth});"
-            end
-        }
+    condition = lhs.map { |stack_and_symbol|
+      stack, symbol = stack_and_symbol
+      depth = stack_depths[stack.interned]
+      stack_depths[stack.interned] += 1
+      "stack_peek(&stacks[#{stack.interned}] /* #{stack.value} */, #{depth}) == #{symbol.interned} /* #{symbol.value} */"
+    }.join("\n          && ")
+    emit "    if(#{condition}) {"
+    stack_depths.each_with_index { |depth, i|
+      if depth != 0
+        emit "        stack_pop(&stacks[#{i}], #{depth});"
+      end
+    }
 
-        rhs.each { |stack_and_symbol|
-            stack, symbol = stack_and_symbol
-            emit "        stack_push(&stacks[#{stack.interned}] /* #{stack.value} */, #{symbol.interned} /* #{symbol.value} */);"
-        }
+    rhs.each { |stack_and_symbol|
+      stack, symbol = stack_and_symbol
+      emit "        stack_push(&stacks[#{stack.interned}] /* #{stack.value} */, #{symbol.interned} /* #{symbol.value} */);"
+    }
 
-        emit "        return 1;"
-        emit "    }"
-        emit ""
-    end
+    emit "        return 1;"
+    emit "    }"
+    emit ""
+  end
 
-    def gen_rules
-        @ast.each_with_index { |rule, n| 
-            if !rule.lhs.value.empty?
-                gen_rule n, rule.lhs.value, rule.rhs.value
-            end
-        }
-    end
+  def gen_rules
+    @ast.each_with_index { |rule, n|
+      if !rule.lhs.value.empty?
+        gen_rule n, rule.lhs.value, rule.rhs.value
+      end
+    }
+  end
 
-    def generate
-        gen_names
-        emit "Stack stacks[#{StackSymbol.counter}];"
-        emit ""
-        emit "void alta_init(void) {"
-        gen_stacks_init
-        emit "}"
-        emit ""
+  def generate
+    gen_names
+    emit "Stack stacks[#{StackSymbol.counter}];"
+    emit ""
+    emit "void alta_init(void) {"
+    gen_stacks_init
+    emit "}"
+    emit ""
 
-        emit "int alta_iteration() {"
-        gen_rules
-        emit "    return 0;"
-        emit "}"
-        emit ""
+    emit "int alta_iteration() {"
+    gen_rules
+    emit "    return 0;"
+    emit "}"
+    emit ""
 
-        emit {{ read_file("./c_code/stack_display.c")}}
-        emit ""
-        emit ""
-        emit {{ read_file("./c_code/main.c")}}
+    emit {{ read_file("./c_code/stack_display.c") }}
+    emit ""
+    emit ""
+    emit {{ read_file("./c_code/main.c") }}
 
-        @output_code
-    end
+    @output_code
+  end
 end
--- src/parser.cr
+++ src/parser.cr
@@ -1,229 +1,226 @@
 enum TokenType
-    VerticalBar
-    Symbol
-    Colon
-    Comma
-    EOF
+  VerticalBar
+  Symbol
+  Colon
+  Comma
+  EOF
 end
 
 class Token
-    @type : TokenType
-    @value : String
-    @pos : Int32
+  @type : TokenType
+  @value : String
+  @pos : Int32
 
-    getter :type
-    getter :value
-    getter :pos
+  getter :type
+  getter :value
+  getter :pos
 
-    def initialize(@type, @value, @pos)
-    end
+  def initialize(@type, @value, @pos)
+  end
 
-    def to_s(io : IO)
-        io << "Token(#{@type}, #{@value}, #{@pos})"
-    end
+  def to_s(io : IO)
+    io << "Token(#{@type}, #{@value}, #{@pos})"
+  end
 end
 
 class Lexer
-    @source : String
-    @start : Int32
-    @pos : Int32
+  @source : String
+  @start : Int32
+  @pos : Int32
 
-    def initialize(@source)
-        @start = 0
-        @pos = 0
+  def initialize(@source)
+    @start = 0
+    @pos = 0
+  end
+
+  def look
+    @source[@pos]
+  end
+
+  def advance
+    @pos += 1
+  end
+
+  def selected_token
+    @source[@start...@pos]
+  end
+
+  def skipspace
+    while @pos < @source.size && look.whitespace?
+      advance
     end
+  end
 
-    def look
-        @source[@pos]
+  def next
+    skipspace
+    @start = @pos
+    return Token.new(TokenType::EOF, "EOF", @source.size) if @pos >= @source.size
+    case look
+    when '|'
+      advance
+      Token.new(TokenType::VerticalBar, selected_token, @start)
+    when ':'
+      advance
+      Token.new(TokenType::Colon, selected_token, @start)
+    when ','
+      advance
+      Token.new(TokenType::Comma, selected_token, @start)
+    else
+      while @pos < @source.size && look != '|' && look != ':' && look != ','
+        advance
+      end
+      Token.new(TokenType::Symbol, selected_token.strip, @start)
     end
-
-    def advance
-        @pos += 1
-    end
-
-    def selected_token
-        @source[@start...@pos]
-    end
-
-    def skipspace
-        while @pos < @source.size && look.whitespace?
-            advance
-        end
-    end
-
-    def next
-        skipspace
-        @start = @pos
-        return Token.new(TokenType::EOF, "EOF", @source.size) if @pos >= @source.size
-        case look
-        when '|'
-            advance
-            Token.new(TokenType::VerticalBar, selected_token, @start)
-        when ':'
-            advance
-            Token.new(TokenType::Colon, selected_token, @start);
-        when ','
-            advance
-            Token.new(TokenType::Comma, selected_token, @start)
-        else
-            while @pos < @source.size && look != '|' && look != ':' && look != ','
-                advance
-            end
-            Token.new(TokenType::Symbol, selected_token.strip, @start)
-        end
-    end
-
+  end
 end
 
 class Interned
-    @@interned = {} of String => Int32
-    @@counter = 0
-    @value : String
-    getter :value
-    
-    def initialize(@value)
-        if !@@interned.has_key? @value
-            @@interned[@value] = @@counter
-            @@counter += 1
-        end
-    end
+  @@interned = {} of String => Int32
+  @@counter = 0
+  @value : String
+  getter :value
 
-    def interned
-        @@interned[@value]
+  def initialize(@value)
+    if !@@interned.has_key? @value
+      @@interned[@value] = @@counter
+      @@counter += 1
     end
+  end
 
-    def Interned.counter
-        @@counter
+  def interned
+    @@interned[@value]
+  end
+
+  def Interned.counter
+    @@counter
+  end
+
+  def Interned.get_name(interned_symbol)
+    begin
+      return @@interned.key_for interned_symbol
+    rescue ex : IndexError
+      raise Exception.new "unreachable"
     end
-
-    def Interned.get_name(interned_symbol)
-        begin
-            return @@interned.key_for interned_symbol
-        rescue ex : IndexError
-            raise Exception.new "unreachable"
-        end
-    end
-
+  end
 end
 
 class StackSymbol < Interned
-    def to_s(io : IO)
-        io << "Stack(\"#{@value}\", #{@@interned[@value]})"
-    end
+  def to_s(io : IO)
+    io << "Stack(\"#{@value}\", #{@@interned[@value]})"
+  end
 end
 
 class SimpleSymbol < Interned
-    def to_s(io : IO)
-        io << "Symbol(\"#{@value}\", #{@@interned[@value]})"
-    end
+  def to_s(io : IO)
+    io << "Symbol(\"#{@value}\", #{@@interned[@value]})"
+  end
 end
 
 class Side
-    @value = [] of {StackSymbol, SimpleSymbol}
-    getter :value
+  @value = [] of {StackSymbol, SimpleSymbol}
+  getter :value
 
-    def <<(stack_and_symbol)
-        @value << stack_and_symbol
-    end
+  def <<(stack_and_symbol)
+    @value << stack_and_symbol
+  end
 
-    def to_s(io : IO)
-        io << @value.map{ |e| "#{e[0].to_s}: #{e[1].to_s}" }.join(", ")
-    end
+  def to_s(io : IO)
+    io << @value.map { |e| "#{e[0].to_s}: #{e[1].to_s}" }.join(", ")
+  end
 end
 
 class Rule
-    @lhs : Side
-    @rhs : Side
-    getter :lhs
-    getter :rhs
+  @lhs : Side
+  @rhs : Side
+  getter :lhs
+  getter :rhs
 
-    def initialize(@lhs, @rhs)
-    end
+  def initialize(@lhs, @rhs)
+  end
 
-    def to_s(io : IO)
-        io << "| #{@lhs} | #{@rhs}"
-    end
+  def to_s(io : IO)
+    io << "| #{@lhs} | #{@rhs}"
+  end
 end
 
 class ParseError < Exception
-    def initialize(file_name : String, pos : {Int32, Int32}, msg : String)
-        super("#{file_name}:#{pos[0]}:#{pos[1]}: #{msg}")
-    end
+  def initialize(file_name : String, pos : {Int32, Int32}, msg : String)
+    super("#{file_name}:#{pos[0]}:#{pos[1]}: #{msg}")
+  end
 end
 
 class Parser
-    @lexer : Lexer
-    @look : Token
-    @file_name : String
-    @source : String
+  @lexer : Lexer
+  @look : Token
+  @file_name : String
+  @source : String
 
-    def initialize(@file_name, @source)
-        @lexer = Lexer.new source
-        @look = @lexer.next
-    end
+  def initialize(@file_name, @source)
+    @lexer = Lexer.new source
+    @look = @lexer.next
+  end
 
-    def line_and_column(pos : Int32) : {Int32, Int32}
-        line = 1
+  def line_and_column(pos : Int32) : {Int32, Int32}
+    line = 1
+    column = 1
+
+    @source.each_char_with_index { |c, i|
+      if i == pos
+        return {line, column}
+      end
+      if c == '\n'
+        line += 1
         column = 1
-        
-        @source.each_char_with_index { |c, i|
-            if i == pos
-                return {line, column}
-            end
-            if c == '\n'
-                line += 1
-                column = 1
-            else
-                column += 1
-            end
-        }
-        raise Exception.new "unreachable"
-    end
+      else
+        column += 1
+      end
+    }
+    raise Exception.new "unreachable"
+  end
 
-    def advance
-        result = @look
-        @look = @lexer.next
-        result
-    end
+  def advance
+    result = @look
+    @look = @lexer.next
+    result
+  end
 
-    def match(type : TokenType)
-        raise ParseError.new(@file_name, line_and_column(@look.pos), "expected #{type}") if @look.type != type
+  def match(type : TokenType)
+    raise ParseError.new(@file_name, line_and_column(@look.pos), "expected #{type}") if @look.type != type
+    advance
+  end
+
+  def stack_and_symbol
+    stack = match TokenType::Symbol
+    match TokenType::Colon
+    symbol = match TokenType::Symbol
+    {StackSymbol.new(stack.value), SimpleSymbol.new(symbol.value)}
+  end
+
+  def side
+    result = Side.new
+    if @look.type == TokenType::Symbol
+      result << stack_and_symbol
+      while @look.type == TokenType::Comma
         advance
+        result << stack_and_symbol
+      end
     end
+    return result
+  end
 
-    def stack_and_symbol
-        stack = match TokenType::Symbol
-        match TokenType::Colon
-        symbol = match TokenType::Symbol
-        {StackSymbol.new(stack.value), SimpleSymbol.new(symbol.value)}
+  def rule
+    match TokenType::VerticalBar
+    lhs = side
+    match TokenType::VerticalBar
+    rhs = side
+    Rule.new lhs, rhs
+  end
+
+  def parse
+    rules = [] of Rule
+    while @look.type != TokenType::EOF
+      rules << rule
     end
-
-    def side
-        result = Side.new
-        if @look.type == TokenType::Symbol
-            result << stack_and_symbol
-            while @look.type == TokenType::Comma
-                advance
-                result << stack_and_symbol
-            end
-        end
-        return result
-    end
-
-    def rule
-        match TokenType::VerticalBar
-        lhs = side
-        match TokenType::VerticalBar
-        rhs = side
-        Rule.new lhs, rhs
-    end
-
-    def parse
-        rules = [] of Rule
-        while @look.type != TokenType::EOF
-            rules << rule
-        end
-        rules
-    end
-
+    rules
+  end
 end
